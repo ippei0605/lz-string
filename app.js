a38f463984c1c5b876115d19d8121dab0452a54a
Ippei SUZUKI <ippei0605@gmail.com>
2020-04-09 09:00:04 +0900

    リファクタリング

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const LZString = require('lz-string');
 
+const print = (input, output, valid) => {
+  console.log(`length: ${input.length} --> ${output.length} (${output.length / input.length}), valid: ${input === valid}`);
+};
+
 let input, output, valid;
 
 // 1
@@ -9,7 +13,7 @@ input = 'text,part_of_speech,tokens,readings\n' +
   '動画コース,カスタム名詞,動画コース,動画コース';
 output = LZString.compress(input);
 valid = LZString.decompress(output);
-console.log(input.length, output.length, input === valid);
+print(input, output, valid);
 
 // 2
 const json = [
@@ -29,4 +33,4 @@ const json = [
 input = JSON.stringify(json);
 output = LZString.compress(input);
 valid = LZString.decompress(output);
-console.log(input.length, output.length, input === valid);
+print(input, output, valid);
